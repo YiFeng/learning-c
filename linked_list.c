616076d4576d44e329c4041f60361c05c63542cb
YiFeng <yif12@uci.edu>
2021-01-02 21:45:57 -0800

    add find, insert, and delete

--- linked_list.c
+++ linked_list.c
@@ -7,7 +7,6 @@
  *  5. Print all elements in the linked list
  */
 
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -57,16 +56,43 @@ void print_linked_list(LinkedListNode *head){
     printf("\n");
 }
 
-void linked_list_insert(LinkedListNode *node, LinkedListNode *new_node) {
-
+void linked_list_insert(LinkedListNode *node, LinkedListNode *new_node){
+    LinkedListNode *next_node = node->next;
+    node->next = new_node;
+    new_node->next = next_node;
 }
 
-void linked_list_find(LinkedListNode *head, int value) {
-
+LinkedListNode *linked_list_find_first(LinkedListNode *head, int value) {
+    LinkedListNode *current_node = head;
+    while (current_node != NULL){
+        if(current_node->data == value){
+            return current_node;
+        }
+        current_node = current_node->next;
+    }
+    return NULL;
 }
+
+// LinkedListNode *linked_list_find_first(LinkedListNode *head, int value) {
+//     LinkedListNode *current_node = head;
+//     LinkedListNode *result = NULL;
+//     while (current_node != NULL){
+//         if(current_node->data = value){
+//             result = current_node;
+//             break;
+//         }
+//         current_node = current_node->next;
+//     }
+//     return result;
+// }
 
 void linked_list_delete_next(LinkedListNode *node) {
-
+    if(node->next == NULL){
+        return;
+    }
+    LinkedListNode *delete_node = node->next;
+    node->next = delete_node->next;
+    free(delete_node);
 }
 
 void free_linked_list(LinkedListNode* head) {
@@ -82,5 +108,13 @@ void free_linked_list(LinkedListNode* head) {
 int main(){
     LinkedListNode *head = initialize_linked_list();
     print_linked_list(head);
+    // ##############
+    LinkedListNode *target_node = linked_list_find_first(head, 5);
+    LinkedListNode *inserted_node = creat_node(100);
+    linked_list_insert(target_node, inserted_node);
+    print_linked_list(head);
+    linked_list_delete_next(target_node);
+    print_linked_list(head);
+    // ##############
     free_linked_list(head);
 }
